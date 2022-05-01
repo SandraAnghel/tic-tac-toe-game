0df9ac7848d37069467845dc26d06b0efb6eb785
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-01 16:33:34 +0100

    Add functionality for square button

--- app/src/components/Square.js
+++ app/src/components/Square.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default class Square extends React.Component {
+
     render() {
         console.log(this.props);
         return (
-            <button className="square">
+            <button className="square" onClick={
+                function () {
+                    console.log('click');
+                }}>
                 {this.props.value}
             </button>
         );
